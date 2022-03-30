581f28bd5f25c3e421f8b6e2b17736e63793f6d5
Nidal2022 <nidal.mhamdi93@gmail.com>
2022-03-30 10:37:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import mimageInSrc from "./imageInSrc.jpg"
+
+
+function App() {
+  return (
+    <div>
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+      <h1 className="title red">Nidhal</h1>
+      <br />
+      <img src={mimageInSrc} alt ='myImage' />
+      <br />
+      <img className='imagepublic' src="/imageInPublic.jpg" />
+    </div>
+    <video width={320} height={240} controls>
+      <source src="myVideo.mp4" type="video/mp4" />
+    </video>
+  </div>
+  
+  );
+}
+
+export default App;
